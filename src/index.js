6e8b6aed02f5d27ef3e71a6a0c8fd82a751dc324
RS <rubenstoesser@yahoo.co.uk>
2018-06-14 19:46:11 +0200

    unregister service worker for now

--- src/index.js
+++ src/index.js
@@ -4,5 +4,10 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { unregister } from './registerServiceWorker';
+
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+//registerServiceWorker();
+unregister();
